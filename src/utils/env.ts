56704132dace6ff7d69334fb26cbbc74d4b75c2c
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-05-03 12:38:44 +0530

    changed env value

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,7 +2,7 @@ export const env = {
   API_ENDPOINT:
     import.meta.env.VITE_USER_NODE_ENV === "development"
       ? "http://localhost:8080/api"
-      : "https://trading-api.digisole.in/api",
+      : "https://ksp-api.digisole.in/api",
   MAIN_URL:
     import.meta.env.VITE_USER_NODE_ENV === "development"
       ? "http://localhost:5173"
